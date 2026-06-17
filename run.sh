59968380c81ef3257de9b773364032885bc5edf3
hoptalert <hziv@optalert.com>
2026-06-17 17:25:11 +1000

    exposing port 8888 for Jupyter

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --name docker-template -it --rm -v "$PWD:/workspace" docker-template:latest
+docker run -p 8888:8888 --name docker-template -it --rm -v "$PWD:/workspace" docker-template:latest
